4f278886634dcff6d097468c94ad55850ccea3ec
Brandon Titus <bjtitus@gmail.com>
2012-10-23 09:58:09 -0500

    Disable the recent command since syntax isn't correct and add TODO for pygments installation check

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -14,13 +14,15 @@
 # `cat` with beautiful colors. requires Pygments installed.
 # 							   sudo easy_install Pygments
 # Pulled from github.com/paulirish/dotfiles
+#TODO: Check for pygments installation and warn if it isn't installed
 alias c='pygmentize -O style=monokai -f console256 -g'
 
 # List only directories
 alias lsd='ls -l | grep "^d"'
 
 # Lists recent commands sorted by frequency with a count
-alias recent='history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head'
+# Don't have time to check this and it isn't valid
+# alias recent='history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head'
 
 # Lists all processes using the network
 alias net-apps="lsof -P -i -n"
